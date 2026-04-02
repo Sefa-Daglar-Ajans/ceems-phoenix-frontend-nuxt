f7d3419c3e324c8a14056d2e898b22a4df6ee1ed
13uraks <burak.sahin@pusula.com.tr>
2026-04-02 19:24:10 +0300

    editor reverted to esm

--- app/components/WysiwygEditor.client.vue
+++ app/components/WysiwygEditor.client.vue
@@ -1,5 +1,46 @@
 <script setup lang="ts">
 import Editor from '@tinymce/tinymce-vue'
+import tinymce from 'tinymce/tinymce'
+import 'tinymce/icons/default/icons.min.js'
+import 'tinymce/themes/silver/theme.min.js'
+import 'tinymce/models/dom/model.min.js'
+import 'tinymce/skins/ui/oxide/skin.js'
+import 'tinymce/skins/ui/oxide/content.js'
+import 'tinymce/skins/content/default/content.js'
+
+// Ensure global is available for @tinymce/tinymce-vue detection in production builds
+if (typeof window !== 'undefined') {
+  ;(window as any).tinymce = tinymce
+}
+
+import 'tinymce/plugins/accordion'
+import 'tinymce/plugins/advlist'
+import 'tinymce/plugins/anchor'
+import 'tinymce/plugins/autolink'
+import 'tinymce/plugins/autoresize'
+import 'tinymce/plugins/charmap'
+import 'tinymce/plugins/code'
+import 'tinymce/plugins/codesample'
+import 'tinymce/plugins/directionality'
+import 'tinymce/plugins/emoticons'
+import 'tinymce/plugins/emoticons/js/emojis.js'
+import 'tinymce/plugins/fullscreen'
+import 'tinymce/plugins/help'
+import 'tinymce/plugins/image'
+import 'tinymce/plugins/importcss'
+import 'tinymce/plugins/insertdatetime'
+import 'tinymce/plugins/link'
+import 'tinymce/plugins/lists'
+import 'tinymce/plugins/media'
+import 'tinymce/plugins/nonbreaking'
+import 'tinymce/plugins/pagebreak'
+import 'tinymce/plugins/preview'
+import 'tinymce/plugins/quickbars'
+import 'tinymce/plugins/searchreplace'
+import 'tinymce/plugins/table'
+import 'tinymce/plugins/visualblocks'
+import 'tinymce/plugins/visualchars'
+import 'tinymce/plugins/wordcount'
 
 const model = defineModel<string>({ default: '' })
 
@@ -12,6 +53,9 @@ const editorInit = {
   branding: false,
   min_height: 500,
   resize: true,
+  skin: false,
+  content_css: 'default',
+  skin_url: 'default',
 
   plugins: [
     'accordion', 'advlist', 'anchor', 'autolink', 'autoresize',
